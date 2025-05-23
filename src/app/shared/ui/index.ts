0646d15b335d3a5db6355df4575a11edbf01c2fc
Paweł Morawczyński <burciak01@gmail.com>
2025-05-23 23:36:06 +0200

    feat: add irreversible operation tag UI component

--- src/app/shared/ui/index.ts
+++ src/app/shared/ui/index.ts
@@ -1,3 +1,4 @@
 export * from './label/label.component';
 export * from './side-nav/side-nav.component';
 export * from './user-layout/user-layout.component';
+export * from './irreversible-operation-tag/irreversible-operation-tag.component';
